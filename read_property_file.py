f69e8deaaddc63887b0a4f9db8fbfacc1c3e71bf
Mikhilesh sekhar <mikepro360@outlook.com>
2017-08-31 17:09:53 +0530

    Update read_property_file.py

--- read_property_file.py
+++ read_property_file.py
@@ -1,19 +1,23 @@
 import os
 
 def read_properties(file,ky):
+
 	"""
+	
 	Method to read values from the property file.This method converts the values of the file into python dictionary as key pair value 
-  and parses it and returns the value.
-  Usage as below:-
-    obj = read_properties(FILE_NAME,KEY)
-    FILE_NAME:- Name of the file, or the file name along with the path
-    KEY:- Supply the key for getting the corresponding value for it in the 'obj' variable . 
+	@Args:- file, ky
+	@Return:- d[ky]
+	and parses it and returns the value.
+	Usage as below:-
+		obj = read_properties(FILE_NAME,KEY)
+		FILE_NAME:- Name of the file, or the file name along with the path
+		KEY:- Supply the key for getting the corresponding value for it in the 'obj' variable . 
   
 	Files with variable and value for it.
-  Sample of the file as follows:-
-    one=1
-    name=mike
-    age=23
+	Sample of the file as follows:-
+		one=1
+		name=mike
+		age=23
 	"""
   
 	d = {}	
